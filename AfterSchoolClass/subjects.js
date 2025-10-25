b0ea43b562e342fc406839e8a0b65fa73029ac0d
SaiNeetiSayam <SS4608@live.mdx.ac.uk>
2025-10-25 22:55:28 +0400

    Created basic front-end for After School Class App

---
+++ AfterSchoolClass/subjects.js
@@ -0,0 +1,42 @@
+             let subjects = [
+                        {
+                            id: 1001,
+                            title: "Math",
+                            location: "London",
+                            price: 100,
+                            image: "../assets/images/math-london.png",
+                            availableSpaces: 5,
+                        },
+                        {
+                            id: 1002,
+                            title: "Math",
+                            location: "Oxford",
+                            price: 100,
+                            image: "../assets/images/math-oxford.png",
+                            availableSpaces: 5,
+                        },
+                        {
+                            id: 1003,
+                            title: "English",
+                            location: "London",
+                            price: 200,
+                            image: "../assets/images/english-london.png",
+                            availableSpaces: 10,
+                        },
+                        {
+                            id: 1004,
+                            title: "English",
+                            location: "Cambridge",
+                            price: 100,
+                            image: "../assets/images/english-cambridge.png",
+                            availableSpaces: 12,
+                        },
+                        {
+                            id: 1005,
+                            title: "Music",
+                            location: "Bristol",
+                            price: 90,
+                            image: "../assets/images/music-bristol.png",
+                            availableSpaces: 5,
+                        },
+                    ]
