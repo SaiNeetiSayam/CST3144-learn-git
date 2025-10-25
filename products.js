f3be1e434e4595f4363d20da80c6446decf5e93e
SaiNeetiSayam <SS4608@live.mdx.ac.uk>
2025-10-25 21:32:32 +0400

    Added six products in an array and handled the front-end to manage cart and checkout

---
+++ products.js
@@ -0,0 +1,56 @@
+             let products = [
+                        {
+                            id: 1001,
+                            title: "Cat Food, 25lb bag",
+                            description: "A 25 pound bag of <em>irresistable</em>, "+"organic goodness for your cat.",
+                            price: 2000,
+                            image: "assets/images/product-fullsize.png",
+                            availableInventory: 10,
+                            rating: 3
+                        },
+                        {
+                            id: 1002,
+                            title: "Cat-House",
+                            description: "A <em>beautiful home</em>, "+"for your cat.",
+                            price: 3000,
+                            image: "assets/images/cat-house.jpg",
+                            availableInventory: 5,
+                            rating: 4
+                        },
+                        {
+                            id: 1003,
+                            title: "Cat Litter, 10lb bag",
+                            description: "A 10 pound bag of Cat Litter.",
+                            price: 500,
+                            image: "assets/images/cat-litter.jpg",
+                            availableInventory: 20,
+                            rating: 2
+                        },
+                        {
+                            id: 1004,
+                            title: "Laser Pointer",
+                            description: "A <em>powerful</em>, "+"light for your pet.",
+                            price: 4000,
+                            image: "assets/images/laser-pointer.jpg",
+                            availableInventory: 15,
+                            rating: 5
+                        },
+                        {
+                            id: 1005,
+                            title: "Yarn",
+                            description: "A <em>yarn</em>, "+"for your pet.",
+                            price: 800,
+                            image: "assets/images/yarn.jpg",
+                            availableInventory: 15,
+                            rating: 5
+                        },
+                        {
+                            id: 1006,
+                            title: "Mindy Mouse",
+                            description: "A <em>playful</em>, "+"toy for your cat.",
+                            price: 800,
+                            image: "assets/images/Mindy_Mouse_cat_toy.jpg",
+                            availableInventory: 15,
+                            rating: 5
+                        }
+                    ]
